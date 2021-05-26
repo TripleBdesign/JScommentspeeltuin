b41f9e833cfb72fb301a46363a0b857aa71874c0
Bianca <84011677+TripleBdesign@users.noreply.github.com>
2021-05-26 15:53:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("Hallo Winc Academy studenten");
+let name = 'Bianca';
+
+//Deel 2 vermenigvuldigen * - Dit is een grote som
+let multiplication = 1230941 * 1823794;
+console.log("vermenigvuldiging:", multiplication);
+
+//Deel 2 delen /
+let division = 637263 / 54;
+console.log("deelsom:", division);
+
+
+
+//vraag 4 antwoord: ja dat zie ik.
+
+/* Dit is een blok comment
+om de 2 voorgaande rekensommen in 1 comment te zetten */
+
+
